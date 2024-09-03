8cdd2616bae60ac143768b0cee6aabec190aae37
Maksim <malcewdeveloper@yandex.ru>
2024-09-03 23:04:53 +0300

    fix: исправляет ошибки линтинга

--- packages/client/sw.js
+++ packages/client/sw.js
@@ -1,7 +1,7 @@
+/* global navigator, window, self, caches, console, fetch */
+
 const CACHE_NAME = "lazer-overload-cache-v1";
-const URLS = [
-    "src/main.tsx"
-];
+const URLS = ["src/main.tsx"];
 
 function startServiceWorker() {
     if ("serviceWorker" in navigator) {
@@ -39,7 +39,8 @@ self.addEventListener("install", (event) => {
 // Перехват запросов и работа с кэшом
 self.addEventListener("fetch", (event) => {
     event.respondWith(
-        caches.match(event.request)
+        caches
+            .match(event.request)
             .then((response) => {
                 if (response) {
                     return response; // Возвращаем ресурс из кэша
@@ -48,7 +49,11 @@ self.addEventListener("fetch", (event) => {
                 // Если ресурса нет в кэше, делаем запрос на сервер
                 return fetch(event.request).then((networkResponse) => {
                     // Проверяем, был ли ответ корректным
-                    if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+                    if (
+                        !networkResponse ||
+                        networkResponse.status !== 200 ||
+                        networkResponse.type !== "basic"
+                    ) {
                         return networkResponse;
                     }
 
@@ -62,9 +67,12 @@ self.addEventListener("fetch", (event) => {
                 });
             })
             .catch((error) => {
-                console.error("Fetch failed; returning offline page instead.", error);
+                console.error(
+                    "Fetch failed; returning offline page instead.",
+                    error,
+                );
                 // Здесь можно вернуть специальную офлайн-страницу или другой ресурс
-            })
+            }),
     );
 });
 
@@ -75,12 +83,15 @@ self.addEventListener("activate", (event) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
                     if (cacheName !== CACHE_NAME) {
-                        console.log("[Service Worker] Deleting old cache:", cacheName);
+                        console.log(
+                            "[Service Worker] Deleting old cache:",
+                            cacheName,
+                        );
                         return caches.delete(cacheName);
                     }
-                })
+                }),
             );
-        })
+        }),
     );
 });
 
