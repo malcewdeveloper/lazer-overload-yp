aaabccb2062bc189caa9912d4ead3a6c8441a7d1
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-09-05 16:27:45 +0300

    feat: Добавить ещё одно WEB API (кроме Canvas, Fetch)

--- packages/client/src/pages/Game/index.tsx
+++ packages/client/src/pages/Game/index.tsx
@@ -1,17 +1,36 @@
-import React from "react";
-import { Flex, Layout, Typography } from "antd";
+import React, { useState } from "react";
+import { Flex, Layout, Typography, Button } from "antd";
 import CanvasGame from "./CanvasGame";
 import classes from "./Game.module.scss";
+import { ArrowsAltOutlined, ShrinkOutlined } from "@ant-design/icons";
 
 const { Content } = Layout;
 
 export const Game: React.FC<object> = () => {
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    const handleFullscreen = () => {
+        if (!isFullscreen) {
+            document.documentElement.requestFullscreen();
+        } else if (document.exitFullscreen) {
+            document.exitFullscreen();
+        }
+        setIsFullscreen(!isFullscreen);
+    };
+
     return (
         <Layout className={classes.root}>
             <Content>
                 <Typography.Title level={1} className={classes.title}>
                     Game
                 </Typography.Title>
+
+                <Button
+                    className={classes.button}
+                    onClick={() => handleFullscreen()}
+                >
+                    {isFullscreen ? <ShrinkOutlined /> : <ArrowsAltOutlined />}
+                </Button>
+
                 <Flex align="center" justify="center">
                     <CanvasGame />
                 </Flex>
