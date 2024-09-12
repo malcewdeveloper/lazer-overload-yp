8a1002e861325816e8b6e42706ac692f69d776b5
Maksim <malcewdeveloper@yandex.ru>
2024-09-12 03:09:27 +0300

    fix: добавил готовое решение для SW

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Router } from "react-router-dom";
 import "./App.css";
 import { history } from "./service";
--- packages/client/src/service/Notification/index.ts
+++ packages/client/src/service/Notification/index.ts
@@ -1,3 +1,5 @@
+import { TNotificationOpen } from "./type";
+
 class NotificationClass {
     status: NotificationPermission = "default";
 
