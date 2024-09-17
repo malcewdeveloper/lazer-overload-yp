600e768b640a3c05cb9400ffb700a234fc32d25f
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-09-17 22:38:07 +0300

    feat: Переключение темы (сервер) #65

---
+++ packages/server/routes/theme.ts
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import {
+    createTheme,
+    getAllThemeSite,
+    getMyTheme,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+} from "../../controllers/theme/theme.js";
+
+export const themesRoutes = (router: Router) => {
+    const themesRouter: Router = Router();
+
+    themesRouter
+        .post("/", createTheme)
+        .get("/:userId", getMyTheme)
+        .get("/", getAllThemeSite);
+
+    router.use("/theme", themesRouter);
+};
