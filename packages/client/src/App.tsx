5489bc52790dd0fee38ede736edd3fa9df0f001e
Svetlana Prozhirova <svetakesha@yandex.ru>
2024-09-17 22:09:05 +0300

    fix: исправления

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,9 +3,12 @@ import "./App.css";
 import { history } from "./service";
 import { ConfigProvider, theme } from "antd";
 import { Routes } from "./pages";
+import { ITheme, useTheme } from "./entities/theme";
 
 function App() {
-    const isDark = Math.random() > 0.5;
+    // const isDark = Math.random() > 0.5;
+    const isDark = useTheme((state: ITheme) => state.isDark);
+    console.log("isDark?", isDark);
 
     return (
         <Router history={history}>
